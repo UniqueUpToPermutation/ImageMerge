d655728bfff1a4d77d9d98538bf55eb8f3cf1a1d
ConArtist141 <irrationalthinking@msn.com>
2015-12-29 13:18:52 -0500

    Fixing stuff

--- CImageMerge/main.cpp
+++ CImageMerge/main.cpp
@@ -2,14 +2,15 @@
 #include <fstream>
 #include <vector>
 #include <assert.h>
+#include <string>
 
 #include "lodepng.h"
 #include "CutGrid.h"
 
-#define IMAGE_SOURCE_1 "goat2.png"
-#define IMAGE_SOURCE_2 "cat.png"
-#define IMAGE_OUTPUT "result.png"
-#define STITCH_MARGIN 100
+#define DEFAULT_IMAGE_SOURCE_1 "goat2.png"
+#define DEFAULT_IMAGE_SOURCE_2 "cat.png"
+#define DEFAULT_IMAGE_OUTPUT "result.png"
+#define DEFAULT_STITCH_MARGIN 100
 #define GRID_ID(x, y, width) 2 + y * width + x
 
 using namespace std;
@@ -18,22 +19,25 @@ using namespace std;
 class EdgeCostSingleton
 {
 public:
-	static const vector<vector<float> >* Image1;
-	static const vector<vector<float> >* Image2;
-	static int Margin;
-	static double LargeNumber;
+	static struct DataObject
+	{
+		const vector<vector<float> >* Image1;
+		const vector<vector<float> >* Image2;
+		int Margin;
+		double LargeNumber;
+	} Data;
 
 	static CapType EdgeCost(int row, int col, CutGrid::EDir dir) {
 		// Edge weights on either side should be infinite
 		if (col == 0 && (dir == CutGrid::DIR_NORTH || dir == CutGrid::DIR_SOUTH))
-			return LargeNumber;
-		else if (col == Margin - 1 && (dir == CutGrid::DIR_NORTH || dir == CutGrid::DIR_SOUTH))
-			return LargeNumber;
+			return Data.LargeNumber;
+		else if (col == Data.Margin - 1 && (dir == CutGrid::DIR_NORTH || dir == CutGrid::DIR_SOUTH))
+			return Data.LargeNumber;
 
 		// Otherwise, take the sum of the absolute differences between the pixels
 		int dx = 0;
 		int dy = 0;
-		int image1Offset = (*Image1)[0].size() - Margin;
+		size_t image1Offset = (*Data.Image1)[0].size() - Data.Margin;
 
 		switch (dir)
 		{
@@ -55,12 +59,15 @@ public:
 		int col2 = col + dx;
 
 		double weight = 0.0;
-		weight += std::fabs((*Image1)[row][image1Offset + col] - (*Image2)[row2][col2]);
-		weight += std::fabs((*Image1)[row2][image1Offset + col2] - (*Image2)[row][col]);
+		weight += std::fabs((*Data.Image1)[row][image1Offset + col] - (*Data.Image2)[row2][col2]);
+		weight += std::fabs((*Data.Image1)[row2][image1Offset + col2] - (*Data.Image2)[row][col]);
 
 		return weight;
 	}
 };
+
+// Singleton data definition
+EdgeCostSingleton::DataObject EdgeCostSingleton::Data;
 
 // Stitch two images together using basic min-cut method
 void performStitching(const vector<vector<float> >& image1,
@@ -68,13 +75,13 @@ void performStitching(const vector<vector<float> >& image1,
 	const int margin, vector<vector<float> >& output)
 {
 	int gridWidth = margin;
-	int gridHeight = image1.size();
+	int gridHeight = static_cast<int>(image1.size());
 
 	// Prepare the singleton edge weight class
-	EdgeCostSingleton::Image1 = &image1;
-	EdgeCostSingleton::Image2 = &image2;
-	EdgeCostSingleton::Margin = margin;
-	EdgeCostSingleton::LargeNumber = 1000000.0 * gridWidth * gridHeight;
+	EdgeCostSingleton::Data.Image1 = &image1;
+	EdgeCostSingleton::Data.Image2 = &image2;
+	EdgeCostSingleton::Data.Margin = margin;
+	EdgeCostSingleton::Data.LargeNumber = 1000000.0 * gridWidth * gridHeight;
 
 	// Run maxflow computation
 	CutGrid grid(gridHeight, gridWidth);
@@ -84,27 +91,26 @@ void performStitching(const vector<vector<float> >& image1,
 	grid.getMaxFlow();
 
 	// Generate output
-	int image1Offset = image1[0].size() - margin;
+	int image1Offset = static_cast<int>(image1[0].size()) - margin;
 	for (int y = 0; y < gridHeight; ++y)
 	{
 		output.push_back(vector<float>(image1[0].size() + image2[0].size() - margin));
-		vector<float>* vecRef = &output.data()[output.size() - 1];
-		float* ptr = vecRef->data();
+		vector<float>& row = output[output.size() - 1];
 
 		// Copy over image 1
 		for (int x = 0; x < image1Offset; ++x)
-			ptr[x] = image1[y][x];
+			row[x] = image1[y][x];
 
 		// Copy over the margin between the images
 		for (unsigned int x = image1Offset; x < image1[0].size(); ++x)
 			if (grid.getLabel(y, x - image1Offset) == CutPlanar::LABEL_SOURCE)
-				ptr[x] = image1[y][x];
+				row[x] = image1[y][x];
 			else
-				ptr[x] = image2[y][x - image1Offset];
+				row[x] = image2[y][x - image1Offset];
 
 		// Copy over image 2
 		for (unsigned int x = 0; x < image2[0].size() - margin; ++x)
-			ptr[x + image1[0].size()] = image2[y][x + margin];
+			row[x + image1[0].size()] = image2[y][x + margin];
 	}
 }
 
@@ -115,11 +121,10 @@ void convertImageDataToFloatMatrix(const vector<unsigned char>& image,
 	for (int y = 0; y < height; ++y)
 	{
 		output.push_back(vector<float>(width));
-		vector<float>* vecRef = &output.data()[output.size() - 1];
-		float* ptr = vecRef->data();
+		vector<float>& row = output[output.size() - 1];
 
 		for (int x = 0; x < width; ++x)
-			ptr[x] = (float)image[4 * (x + y * width)] / 255.0f;
+			row[x] = (float)image[4 * (x + y * width)] / 255.0f;
 	}
 }
 
@@ -140,18 +145,56 @@ void convertFloatMatrixToImageData(const vector<vector<float> >& matrix,
 		}
 }
 
-int main()
+unsigned int floatMatrixFromPNG(const string& filename, vector<vector<float> >& output)
 {
-	// Open the PNG files for image 1 and image 2
-	vector<unsigned char> imageData1;
-	unsigned int imageWidth1;
-	unsigned int imageHeight1;
-	unsigned int error1 = lodepng::decode(imageData1, imageWidth1, imageHeight1, IMAGE_SOURCE_1);
+	// Open the raw PNG data
+	vector<unsigned char> imageData;
+	unsigned int imageWidth;
+	unsigned int imageHeight;
+	unsigned int error = lodepng::decode(imageData, imageWidth, imageHeight, filename);
 
-	vector<unsigned char> imageData2;
-	unsigned int imageWidth2;
-	unsigned int imageHeight2;
-	unsigned int error2 = lodepng::decode(imageData2, imageWidth2, imageHeight2, IMAGE_SOURCE_2);
+	if (error)
+		return error;
+
+	// Convert data to a float matrix
+	convertImageDataToFloatMatrix(imageData, imageWidth, imageHeight, output);
+	return 0;
+}
+
+unsigned int saveFloatMatrixToPNG(const string& filename, const vector<vector<float> >& data)
+{
+	vector<unsigned char> outputImageData;
+	convertFloatMatrixToImageData(data, outputImageData);
+
+	// Save the resulting image
+	return lodepng::encode(filename, outputImageData, static_cast<unsigned int>(data[0].size()), 
+		static_cast<unsigned int>(data.size()));
+}
+
+int main(int argc, char** argv)
+{
+	// Read command line inputs if specified
+	string imageSource1 = DEFAULT_IMAGE_SOURCE_1;
+	string imageSource2 = DEFAULT_IMAGE_SOURCE_2;
+	string outputPath = DEFAULT_IMAGE_OUTPUT;
+	int stitchMargin = DEFAULT_STITCH_MARGIN;
+
+	if (argc >= 2)
+	{
+		imageSource1 = argv[0];
+		imageSource2 = argv[1];
+	}
+	if (argc >= 3)
+		stitchMargin = stoi(argv[2]);
+	if (argc >= 4)
+		outputPath = argv[3];
+
+	// Open the PNG files for image 1 and image 2
+	vector<vector<float> > imageArray1;
+	vector<vector<float> > imageArray2;
+
+	unsigned int error1 = floatMatrixFromPNG(imageSource1, imageArray1);
+	unsigned int error2 = floatMatrixFromPNG(imageSource2, imageArray2);
 
 	if (error1 || error2)
 	{
@@ -159,27 +202,17 @@ int main()
 		return -1;
 	}
 
-	// Convert the image data to float matrices
-	vector<vector<float> > imageArray1;
-	vector<vector<float> > imageArray2;
-	convertImageDataToFloatMatrix(imageData1, imageWidth1, imageHeight1, imageArray1);
-	convertImageDataToFloatMatrix(imageData2, imageWidth2, imageHeight2, imageArray2);
-
+	// Stitch the images together
 	cout << "Stitching images..." << endl;
 
-	// Stitch the images together
 	vector<vector<float> > output;
-	performStitching(imageArray1, imageArray2, STITCH_MARGIN, output);
+	performStitching(imageArray1, imageArray2, stitchMargin, output);
 
 	cout << "Stitching complete!" << endl;
 	cout << "Saving result..." << endl;
 
-	// Convert the result to an 8-bit image array
-	vector<unsigned char> outputImageData;
-	convertFloatMatrixToImageData(output, outputImageData);
-
-	// Save the resulting image
-	unsigned int error = lodepng::encode(IMAGE_OUTPUT, outputImageData, output[0].size(), output.size());
+	// Save the result
+	unsigned int error = saveFloatMatrixToPNG(outputPath, output);
 
 	if (error)
 	{
@@ -191,8 +224,3 @@ int main()
 
 	return 0;
 }
-
-const vector<vector<float> >* EdgeCostSingleton::Image1;
-const vector<vector<float> >* EdgeCostSingleton::Image2;
-int EdgeCostSingleton::Margin;
-double EdgeCostSingleton::LargeNumber;
